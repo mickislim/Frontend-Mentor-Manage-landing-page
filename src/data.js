2ed86713bf701904a451b7fcf33377fecd684094
mickislim <mike.akinyemi41.com>
2024-02-04 14:32:30 -0600

    modified images in build

--- src/data.js
+++ src/data.js
@@ -43,26 +43,26 @@ export const people = [
        name: ` Anisha Li` ,
      text : ` "Manage has supercharged our team’s workflow. The ability to maintain 
      visibility on larger milestones at all times keeps everyone motivated." `,
-     img: './src/assets/images/avatar-anisha.png'
+     img: '/assets/images/avatar-anisha.png'
     },
     {
         name: ` Ali Bravo` ,
       text : ` “We have been able to cancel so many other subscriptions since using 
       Manage. There is no more cross-channel confusion and everyone is much 
       more focused.” `,
-      img: './src/assets/images/avatar-ali.png'
+      img: '/assets/images/avatar-ali.png'
      },
      {
         name: ` Richard Watts` ,
       text : `“Manage allows us to provide structure and process. It keeps us organized 
       and focused. I can’t stop recommending them to everyone I talk to!” `,
-      img: './src/assets/images/avatar-richard.png'
+      img: '/assets/images/avatar-richard.png'
      },
      {
         name: `Shanai Gough ` ,
       text : `“Their software allows us to track, manage and collaborate on our projects 
       from anywhere. It keeps the whole team in-sync without being intrusive.” `,
-      img: './src/assets/images/avatar-shanai.png'
+      img: '/assets/images/avatar-shanai.png'
      },
 ];
 
